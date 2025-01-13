70a8dd7bc70014a5c9144121ff6ad5cac460ab2a
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-13 23:21:33 +0500

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                           <li>How to Buy</li>
                           <li>Corporate & Bulk Purchasing</li>
                           <li>Returns & Refunds</li>
-                          <li>Trendify Shop</li>
+                          <li> Shop</li>
                           <li>Contact Us</li>
                           <li>Purchase Protection</li>
                           <li>Trendify Pick up Points</li>
@@ -27,7 +27,7 @@ const Footer = () => {
 
                   {/* Trendify */}
                   <div>
-                      <h4 className="text-lg font-bold mb-4">Trendify</h4>
+                      <h4 className="text-lg font-bold mb-4">Ecommarce</h4>
                       <ul className="space-y-2 text-sm text-gray-600">
                           <li>About Us</li>
                           <li>Digital Payments</li>
@@ -98,21 +98,21 @@ const Footer = () => {
       <footer className="text-gray-600 body-font font-sans bg-white">
               <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                   <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                      <span className="ml-3 text-xl font-bold">Tredify</span>
+                      <span className="ml-3 text-xl font-bold">Ecommarce</span>
                   </a>
                   <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                       © 2024 Trendify —
                       <a
-                          href="https://github.com/SikandarTahir23"
+                          href="https://github.com/ZuhiiMalakShah"
                           className="text-gray-600 ml-1"
                           rel="noopener noreferrer"
                           target="_blank"
                       >
-                          sikandar.tahir.04@gmail.com
+                         sultansalahuddin295@gmail.com
                       </a>
                   </p>
                   <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                  <Link href="https://www.facebook.com/Arain.Boyz380?mibextid=ZbWKwL" className="ml-3 text-gray-500">
+                  <Link href="" className="ml-3 text-gray-500">
                   <svg
                               fill="currentColor"
                               strokeLinecap="round"
@@ -124,7 +124,6 @@ const Footer = () => {
                               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                           </svg>
                       </Link>
-                      <Link href="https://x.com/SikandarTahir23?s=09" className="ml-3 text-gray-500">
                           <svg
                               fill="currentColor"
                               strokeLinecap="round"
@@ -136,7 +135,6 @@ const Footer = () => {
                               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
                           </svg>
                       </Link>
-                      <Link href="https://www.instagram.com/mrr_sikki?igsh=MTBzMnVhMjB0MmZiMg==" className="ml-3 text-gray-500">
                           <svg
                               fill="none"
                               stroke="currentColor"
@@ -150,7 +148,6 @@ const Footer = () => {
                               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
                           </svg>
                       </Link>
-                      <Link href="https://www.linkedin.com/in/sikandar-tahir-356a56273/" className="ml-3 text-gray-500">
                           <svg
                               fill="currentColor"
                               stroke="currentColor"
